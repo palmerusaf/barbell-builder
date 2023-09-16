e7178eeba0f8d6c67d0427f6b4e251d327457823
Branden Palmer <brandenpalmer08@gmail.com>
2023-09-16 21:52:22 +0700

    finish layout method

--- src/WeightCalc.test.ts
+++ src/WeightCalc.test.ts
@@ -1,15 +1,22 @@
 import { test, expect } from 'vitest';
 
-import { WeightCalc } from './WeightCalc';
+import { WeightCalc } from "./WeightCalc";
 
-test('number below bar weight returns an empty array', () => {
+test('number below bar weight returns error', () => {
   let newWeightCalc = new WeightCalc(0);
-  expect(newWeightCalc.getLayout()).toEqual([])
+  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight too low."))
   newWeightCalc = new WeightCalc(44)
-  expect(newWeightCalc.getLayout()).toEqual([])
+  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight too low."))
 })
 
-test('bar weight returns bar weight', () => {
+test('not divisible by five returns error', () => {
+  let newWeightCalc = new WeightCalc(56);
+  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight not divisible by 5."))
+  newWeightCalc = new WeightCalc(102)
+  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight not divisible by 5."))
+})
+
+test('bar weight returns [0]', () => {
   const barWeight = 45
   const newWeightCalc = new WeightCalc(barWeight);
   expect(newWeightCalc.getLayout()).toEqual([0])
@@ -23,6 +30,31 @@ test('bar weight plus 90 returns 45', () => {
 
 test('bar weight plus 180 returns [45,45]', () => {
   const barWeight = 45
-  const newWeightCalc = new WeightCalc(barWeight + 90);
+  const newWeightCalc = new WeightCalc(barWeight + 180);
   expect(newWeightCalc.getLayout()).toEqual([45, 45])
+})
+
+test('target wight 230 returns [45,45,2.5]', () => {
+  const newWeightCalc = new WeightCalc(230);
+  expect(newWeightCalc.getLayout()).toEqual([45, 45, 2.5])
+})
+
+test('target wight 205 returns [45 , 35 ]', () => {
+  const newWeightCalc = new WeightCalc(205);
+  expect(newWeightCalc.getLayout()).toEqual([45, 35])
+})
+
+test('target wight 185 returns [45 , 25 ]', () => {
+  const newWeightCalc = new WeightCalc(185);
+  expect(newWeightCalc.getLayout()).toEqual([45, 25])
+})
+
+test('target wight 255 returns [45, 45, 10, 5]', () => {
+  const newWeightCalc = new WeightCalc(255);
+  expect(newWeightCalc.getLayout()).toEqual([45, 45, 10, 5])
+})
+
+test('target wight 270 returns [45, 45, 10, 10, 2.5]', () => {
+  const newWeightCalc = new WeightCalc(270);
+  expect(newWeightCalc.getLayout()).toEqual([45, 45, 10, 10, 2.5])
 })
--- src/WeightCalc.ts
+++ src/WeightCalc.ts
@@ -1,10 +1,12 @@
 export class WeightCalc {
   barWeight = 45
-  plateSizes = [45, 35, 25, 5, 2.5];
+  plateSizes = [45, 35, 25, 10, 5, 2.5];
   targetWeight: number;
+
   constructor(targetWeight: number) {
     this.targetWeight = targetWeight;
   }
+
   setTargetWeight(targetWeight: number) {
     this.targetWeight = targetWeight;
   }
@@ -12,17 +14,22 @@ export class WeightCalc {
   /**
    * @returns  An Array that represent the weight layout required for each side of a barbell. This layout should be applied to each side as the array only represent one side.
    */
-  getLayout(): number[] {
-    if (this.targetWeight < this.barWeight) return []
+  getLayout(): number[] | Error {
+    if (this.targetWeight < this.barWeight) return Error("Weight too low.")
+    if (this.targetWeight % 5 !== 0) return Error("Weight not divisible by 5.")
     if (this.targetWeight === this.barWeight) return [0]
     const layoutArray: number[] = []
     let requiredWeight = this.targetWeight - this.barWeight
-    const plateIndex = 0
+    let plateIndex = 0
+    const getSubtractionWeight = () =>
+      this.plateSizes[plateIndex] * 2
+    const lowestPairPlateSize = 5;
 
-
-    if (this.plateSizes[plateIndex] * 2 <= requiredWeight) {
-      layoutArray.push(this.plateSizes[plateIndex])
-      requiredWeight = requiredWeight - this.plateSizes[plateIndex] * 2
+    while (lowestPairPlateSize <= requiredWeight) {
+      if (getSubtractionWeight() <= requiredWeight) {
+        layoutArray.push(this.plateSizes[plateIndex])
+        requiredWeight = requiredWeight - getSubtractionWeight()
+      } else { plateIndex++ }
     }
 
     return layoutArray
