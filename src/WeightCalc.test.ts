bbe61ec3a7023de975290156c4e767c06e6027c3
Branden Palmer <brandenpalmer08@gmail.com>
2023-09-16 22:28:17 +0700

    refactor tests
    refactor method

--- src/WeightCalc.test.ts
+++ src/WeightCalc.test.ts
@@ -1,60 +1,48 @@
 import { test, expect } from 'vitest';
 
-import { WeightCalc } from "./WeightCalc";
+import { getLayout } from "./WeightCalc";
 
 test('number below bar weight returns error', () => {
-  let newWeightCalc = new WeightCalc(0);
-  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight too low."))
-  newWeightCalc = new WeightCalc(44)
-  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight too low."))
+  expect(getLayout(0)).toEqual(new Error("Weight too low."))
+  expect(getLayout(44)).toEqual(new Error("Weight too low."))
 })
 
 test('not divisible by five returns error', () => {
-  let newWeightCalc = new WeightCalc(56);
-  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight not divisible by 5."))
-  newWeightCalc = new WeightCalc(102)
-  expect(newWeightCalc.getLayout()).toEqual(new Error("Weight not divisible by 5."))
+  expect(getLayout(56)).toEqual(new Error("Weight not divisible by 5."))
+  expect(getLayout(102)).toEqual(new Error("Weight not divisible by 5."))
 })
 
-test('bar weight returns [0]', () => {
+test('bar weight returns []', () => {
   const barWeight = 45
-  const newWeightCalc = new WeightCalc(barWeight);
-  expect(newWeightCalc.getLayout()).toEqual([0])
+  expect(getLayout(barWeight)).toEqual([])
 })
 
 test('bar weight plus 90 returns 45', () => {
   const barWeight = 45
-  const newWeightCalc = new WeightCalc(barWeight + 90);
-  expect(newWeightCalc.getLayout()).toEqual([45])
+  expect(getLayout(barWeight + 90)).toEqual([45])
 })
 
 test('bar weight plus 180 returns [45,45]', () => {
   const barWeight = 45
-  const newWeightCalc = new WeightCalc(barWeight + 180);
-  expect(newWeightCalc.getLayout()).toEqual([45, 45])
+  expect(getLayout(barWeight + 180)).toEqual([45, 45])
 })
 
 test('target wight 230 returns [45,45,2.5]', () => {
-  const newWeightCalc = new WeightCalc(230);
-  expect(newWeightCalc.getLayout()).toEqual([45, 45, 2.5])
+  expect(getLayout(230)).toEqual([45, 45, 2.5])
 })
 
 test('target wight 205 returns [45 , 35 ]', () => {
-  const newWeightCalc = new WeightCalc(205);
-  expect(newWeightCalc.getLayout()).toEqual([45, 35])
+  expect(getLayout(205)).toEqual([45, 35])
 })
 
 test('target wight 185 returns [45 , 25 ]', () => {
-  const newWeightCalc = new WeightCalc(185);
-  expect(newWeightCalc.getLayout()).toEqual([45, 25])
+  expect(getLayout(185)).toEqual([45, 25])
 })
 
 test('target wight 255 returns [45, 45, 10, 5]', () => {
-  const newWeightCalc = new WeightCalc(255);
-  expect(newWeightCalc.getLayout()).toEqual([45, 45, 10, 5])
+  expect(getLayout(255)).toEqual([45, 45, 10, 5])
 })
 
 test('target wight 270 returns [45, 45, 10, 10, 2.5]', () => {
-  const newWeightCalc = new WeightCalc(270);
-  expect(newWeightCalc.getLayout()).toEqual([45, 45, 10, 10, 2.5])
+  expect(getLayout(270)).toEqual([45, 45, 10, 10, 2.5])
 })
--- src/WeightCalc.ts
+++ src/WeightCalc.ts
@@ -1,37 +1,26 @@
-export class WeightCalc {
-  barWeight = 45
-  plateSizes = [45, 35, 25, 10, 5, 2.5];
-  targetWeight: number;
+const barWeight = 45
+const plateSizes = [45, 35, 25, 10, 5, 2.5];
 
-  constructor(targetWeight: number) {
-    this.targetWeight = targetWeight;
+/**
+ * @returns  An Array that represent the weight layout required for each side of a barbell. This layout should be applied to each side as the array only represent one side.
+ */
+export function getLayout(targetWeight: number): number[] | Error {
+  if (targetWeight < barWeight) return Error("Weight too low.")
+  if (targetWeight % 5 !== 0) return Error("Weight not divisible by 5.")
+
+  const layoutArray: number[] = []
+  let requiredWeight = targetWeight - barWeight
+  let plateIndex = 0
+  const getSubtractionWeight = () =>
+    plateSizes[plateIndex] * 2
+  const lowestPairPlateSize = plateSizes[plateSizes.length - 1];
+
+  while (lowestPairPlateSize <= requiredWeight) {
+    if (getSubtractionWeight() <= requiredWeight) {
+      layoutArray.push(plateSizes[plateIndex])
+      requiredWeight = requiredWeight - getSubtractionWeight()
+    } else { plateIndex++ }
   }
 
-  setTargetWeight(targetWeight: number) {
-    this.targetWeight = targetWeight;
-  }
-
-  /**
-   * @returns  An Array that represent the weight layout required for each side of a barbell. This layout should be applied to each side as the array only represent one side.
-   */
-  getLayout(): number[] | Error {
-    if (this.targetWeight < this.barWeight) return Error("Weight too low.")
-    if (this.targetWeight % 5 !== 0) return Error("Weight not divisible by 5.")
-    if (this.targetWeight === this.barWeight) return [0]
-    const layoutArray: number[] = []
-    let requiredWeight = this.targetWeight - this.barWeight
-    let plateIndex = 0
-    const getSubtractionWeight = () =>
-      this.plateSizes[plateIndex] * 2
-    const lowestPairPlateSize = 5;
-
-    while (lowestPairPlateSize <= requiredWeight) {
-      if (getSubtractionWeight() <= requiredWeight) {
-        layoutArray.push(this.plateSizes[plateIndex])
-        requiredWeight = requiredWeight - getSubtractionWeight()
-      } else { plateIndex++ }
-    }
-
-    return layoutArray
-  }
+  return layoutArray
 }
