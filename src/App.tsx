b69aece43d6d29a34d9deefed238c959124ca145
Branden Palmer <brandenpalmer08@gmail.com>
2023-09-19 15:57:06 +0700

    integrate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { useState } from "react"
+import { Controls } from "./components/Controls"
+import { Display } from "./components/Display"
+import { Layout } from "./components/Layout"
+import { Title } from "./components/Title"
+import { parseInt } from "lodash"
 
 function App() {
+  const [targetWeight, setTargetWeight] = useState<number>(45)
+
+  const handleChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setTargetWeight(parseInt(e.target.value))
+  }
 
   return (
-    <div className="content-center">hello</div>
+    <div className="bg-slate-400">
+      <Layout title={<Title />}
+        controls={
+          <Controls
+            targetWeight={targetWeight}
+            handleChange={handleChange}
+          />}
+        display={<Display targetWeight={targetWeight} />} />
+    </div>
   )
 }
 
