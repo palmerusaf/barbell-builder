9b57f10599263c0c8c4d064024d88c4f490d8de4
Branden Palmer <brandenpalmer08@gmail.com>
2023-09-19 17:36:15 +0700

    prep for deploy

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
